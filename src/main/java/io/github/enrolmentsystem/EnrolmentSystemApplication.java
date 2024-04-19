c0439b5d642004405cf894d7acca50b8f57494bf
gibranmenezes <gibranquimica@gmail.com>
2024-04-19 07:05:26 -0300

    feat: inital commit

---
+++ src/main/java/io/github/enrolmentsystem/EnrolmentSystemApplication.java
@@ -0,0 +1,13 @@
+package io.github.enrolmentsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrolmentSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnrolmentSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/enrolmentsystem/EnrolmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.enrolmentsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnrolmentSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
